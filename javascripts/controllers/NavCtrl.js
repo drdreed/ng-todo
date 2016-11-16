5f6213ff216cbda079ca867842aaaaa2c25f969a
Dustin Reed <dmreedy1@gmail.com>
2016-11-15 20:17:51 -0600

    installed firebase

---
+++ javascripts/controllers/NavCtrl.js
@@ -0,0 +1,13 @@
+"use strict";
+
+app.controller("NavCtrl", ($scope) => {
+    $scope.navItems = [
+        {
+            name: "Logout"
+        }, {
+            name: "All Items"
+        }, {
+            name: "New Item"
+        }
+    ];
+});
